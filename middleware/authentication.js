301146217a1d07aa2425771464bce3c9640449f7
victor <oguntayovictor65@gmail.com>
2025-02-10 23:14:37 +0100

    add jobScheme and job model

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,20 +1,19 @@
 const { UnauthenticatedError } = require("../errors");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 
-const auth = async (req, res, next) => {
+const auth = async(req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
     throw new UnauthenticatedError("authentication invalid");
-    }
-    const token = authHeader.split(' ')[1]
-    try {
-        const payload = await jwt.verify(token, process.env.JWT_SECRET)
-        req.user = { userId: payload.userID, name: payload.name }
-        next()
-    } catch (error) {
-        throw new UnauthenticatedError('authentication invalid')
-    }
+  }
+  const token = authHeader.split(" ")[1];
+  try {
+    const payload =jwt.verify(token, process.env.JWT_SECRET);
+    req.user = { userId: payload.userID, name: payload.name };
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("authentication invalid");
+  }
 };
 
-
-module.exports = auth 
+module.exports = auth;
