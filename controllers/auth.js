56124a467479b0faab333d305e5aa4c855efdd5e
victor <oguntayovictor65@gmail.com>
2025-02-10 19:22:18 +0100

    generate and send back token in the register controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,14 @@
 const { StatusCodes } = require("http-status-codes");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ user });
+  const token = jwt.sign({ userID: user._id, name: user.name }, "jwtsecret", {
+    expiresIn: "30d",
+  });
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = (req, res) => {
