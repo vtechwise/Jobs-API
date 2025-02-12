4e82349030269e414d03f35d4c459f3ddff6162a
victor <oguntayovictor65@gmail.com>
2025-02-12 16:02:55 +0100

    remove customAppiError check, it is handle by customError now

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,13 +1,10 @@
-const { CustomAPIError } = require("../errors");
 const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
   const customError = {
     statusCode: err.statusCode || 500,
     message: err.message || "Something went wrong",
   };
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
+
   if (err.code && err.code === 11000) {
     customError.statusCode = 400;
     customError.message = `Duplicate value entered for ${Object.keys(
